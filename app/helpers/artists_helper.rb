1cf5cfe5892e1ec37d46169635a540955258fbe9
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-28 00:06:11 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,3 +1,9 @@
 module ArtistsHelper
-  
+  def display_artist(song)
+    if song.artist_name != nil
+      link_to song.artist_name, artist_path(song.artist)
+    else
+      link_to song.name, song_path(edit_song)
+    end
+  end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,9 +1,3 @@
 module SongsHelper
-  def display_artist(song)
-    if song.artist_name != nil
-      link_to song.artist_name, artist_path(song.artist)
-    else
-      link_to song.name, song_path(edit_song)
-    end
-  end
+
 end
